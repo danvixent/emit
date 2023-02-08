8d73310be91e0358bb8c8bcd95bd5a5e34395885
Daniel Oluojomu <danvixent@gmail.com>
2023-02-08 15:24:11 +0000

    feat: add github command

---
+++ github.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/spf13/cobra"
+	"io"
+	"net/http"
+)
+
+func addGithubCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:              "github",
+		Short:            "Emit a github event",
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {},
+		RunE: func(cmd *cobra.Command, args []string) error {
+
+			body := map[string]interface{}{
+				"product_id": "123",
+			}
+
+			url, err := cmd.Flags().GetString("url")
+			if err != nil {
+				return err
+			}
+
+			secret, err := cmd.Flags().GetString("secret")
+			if err != nil {
+				return err
+			}
+
+			if url == "" || secret == "" {
+				return errors.New("url and secret are required to emit github event")
+			}
+			fmt.Println("url", url)
+			fmt.Println("secret", secret)
+
+			b, err := json.Marshal(body)
+			if err != nil {
+				return fmt.Errorf("failed to marshal body: %v", err)
+			}
+
+			buf := bytes.NewBuffer(b)
+			r, err := http.NewRequest(http.MethodPost, url, buf)
+			if err != nil {
+				return fmt.Errorf("failed to create new http request: %v", err)
+			}
+
+			hmacHeader, err := generateGithubHMAC(secret, b)
+			if err != nil {
+				return fmt.Errorf("failed to generate hmac header new http request: %v", err)
+			}
+
+			setGithubHeaders(r, hmacHeader)
+
+			resp, err := http.DefaultClient.Do(r)
+			if err != nil {
+				return fmt.Errorf("failed to send request: %v", err)
+			}
+
+			respBuf, err := io.ReadAll(resp.Body)
+			if err != nil {
+				return fmt.Errorf("failed to decode response body: %v", err)
+			}
+
+			fmt.Printf("response body: \n%s\n", string(respBuf))
+			fmt.Printf("status code: %d", resp.StatusCode)
+
+			return nil
+		},
+		PersistentPostRun: func(cmd *cobra.Command, args []string) {},
+	}
+
+	return cmd
+}
+
+func generateGithubHMAC(secret string, payload []byte) (string, error) {
+	return generateHMAC(secret, payload, "hex")
+}
+
+func setGithubHeaders(r *http.Request, hmacHeader string) {
+	r.Header.Set("X-Hub-Signature-256", "sha256="+hmacHeader)
+}
